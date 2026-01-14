1ca13026ebfe923b588e48a3e21607de758c5f7c
khushbu <pujakumari29122001@gmail.com>
2026-01-14 11:50:29 +0530

    update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,15 +1,21 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000"); // match backend
+// Use the Render backend URL here
+export const socket = io("https://gigflow-backend-a3ke.onrender.com", {
+  withCredentials: true, // allow cookies if backend uses them
+});
 
+// Log connection
 socket.on("connect", () => {
   console.log("Connected to Socket.IO server:", socket.id);
 });
 
+// Listen for new bids
 socket.on("newBid", (data) => {
   console.log("New bid received:", data);
 });
 
+// Function to place a bid
 export const placeBid = (bid) => {
   socket.emit("bidPlaced", bid);
 };
